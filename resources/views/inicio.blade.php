37dde65447268365849d7043334207698d7e5103
miguel <m.alejandro_80@hotmail.com>
2018-08-27 23:24:43 -0400

    creando maqueta de la vista inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,5 +1,38 @@
 @extends('Layout')
 
 @section('content')
-	<h1 class="text-center my-5">inicio</h1>
+<div class="container-fluid text-center">
+
+	<div class="backg d-flex">
+		<div class="mx-auto text-center align-self-center centro">
+			<h1>Encuentra tu especialista y programa una cita, asi de facil es!</h1>
+				<form class="form-inline justify-content-center">
+					 @csrf
+				  <div class="form-group mr-4">
+				    <input type="text" class="form-control "  placeholder="busqueda">
+				  </div>
+
+				  <button type="submit" class="btn btn-primary">buscar</button>
+				</form>
+		</div>
+	</div>
+				<h2>como funciona la pagina</h2>
+		<div class="row">
+			<div class="col-md-4">
+				<div class="bloque"></div>
+				<h2>busca tu especialista</h2>
+			</div>
+			<div class="col-md-4">
+				<div class="bloque"></div>
+				<h2>programa el dia y la hora tu cita</h2>
+			</div>
+			<div class="col-md-4">
+				<div class="bloque"></div>
+				<h2>asiste</h2>
+			</div>
+		</div>
+		<h2>facil y rapido</h2>
+	{{-- <div class="container-fluid text-center">
+	</div> --}}
+</div>
 @endsection
